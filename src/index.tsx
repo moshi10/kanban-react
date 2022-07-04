fc78d82e150e342928bb5ad63a8bdeeb5317dfc4
moshi10 <mamiko.oshino@gmail.com>
2022-07-04 15:50:57 +0900

    fix: chakra-ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,16 @@
-// 1. Import the extendTheme function
-import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-// 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
-
-const theme = extendTheme({ colors })
-
-// 3. Pass the `theme` prop to the `ChakraProvider`
-function App() {
-  return (
-    <ChakraProvider theme={theme}>
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <ChakraProvider>
+    <React.StrictMode>
       <App />
-    </ChakraProvider>
-  )
-}
+    </React.StrictMode>
+  </ChakraProvider>
+);
+
