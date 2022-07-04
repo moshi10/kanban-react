ad6ef705933400aa083b65d206509f6b3693cc38
moshi10 <mamiko.oshino@gmail.com>
2022-07-04 15:10:14 +0900

    add: chakra-ui, prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// 1. Import the extendTheme function
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
 
+const theme = extendTheme({ colors })
+
+// 3. Pass the `theme` prop to the `ChakraProvider`
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  )
+}
